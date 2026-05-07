4701e1c3591a2a777921795255a06ef3479cdf7d
Sanbid Ray <sujitray.lbs.kmc@gmail.com>
2026-05-07 13:53:53 +0530

    fixed db.py for Render depeloyment

--- database/db.py
+++ database/db.py
@@ -8,6 +8,6 @@ def get_db_connection():
         user=config['MYSQL_USER'],
         password=config['MYSQL_PASSWORD'],
         database=config['MYSQL_DB'],
-        port=config['MYSQL_DB_PORT']
+        port=int(config['MYSQL_DB_PORT'])
     )
     return connection
